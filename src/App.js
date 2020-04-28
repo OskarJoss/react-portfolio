241f6cf4f41530627a965bb193da065e57319e6d
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-04-28 16:57:47 +0200

    setup react and contentful

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import client from "./contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 function App() {
+  const [projects, setProjects] = React.useState(null);
+
+  React.useEffect(() => {
+    client
+      .getEntries({
+        content_type: "project",
+      })
+      .then((entries) => {
+        setProjects(entries.items);
+        console.log(entries.items);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {projects &&
+        projects.map((project, i) => {
+          return (
+            <div key={i}>
+              <h1>{project.fields.title}</h1>
+              <img src={project.fields.image.fields.file.url} alt={"hej"}></img>
+              {documentToReactComponents(project.fields.text)}
+            </div>
+          );
+        })}
     </div>
   );
 }
