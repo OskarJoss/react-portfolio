c36221d504011e1a132c210f73e4f3e29673ae62
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-05-03 20:43:51 +0200

    deploy test 04

---
+++ build/precache-manifest.7162ab21ee59aae32a0a925e775558d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32eff92e2f061f4fd88d113c18f20c97",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2e2918c395f917c4a565",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7208640cec91331699dc",
+    "url": "./static/js/2.87a8dde1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.87a8dde1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2918c395f917c4a565",
+    "url": "./static/js/main.dedb0ea8.chunk.js"
+  },
+  {
+    "revision": "9b0a63e06cc9c1fb0b2d",
+    "url": "./static/js/runtime-main.62f01bd4.js"
+  }
+]);
