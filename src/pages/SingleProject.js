03948e21f615f6cc8c7f09a8d037f6a62063c578
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-05-04 14:22:37 +0200

    test14

--- src/pages/SingleProject.js
+++ src/pages/SingleProject.js
@@ -5,32 +5,32 @@ import { Redirect } from "@reach/router";
 import "../App.css";
 
 const SingleProject = (props) => {
-  // const [project, setProject] = React.useState(null);
+  const [project, setProject] = React.useState(null);
 
-  // React.useEffect(() => {
-  //   client
-  //     .getEntries({
-  //       content_type: "project",
-  //       "fields.slug": props.slug,
-  //     })
-  //     .then((entries) => {
-  //       if (entries.items.length !== 0) {
-  //         setProject(entries.items[0].fields);
-  //       } else {
-  //         setProject([]);
-  //       }
-  //     });
-  // }, [props.slug]);
+  React.useEffect(() => {
+    client
+      .getEntries({
+        content_type: "project",
+        "fields.slug": props.slug,
+      })
+      .then((entries) => {
+        if (entries.items.length !== 0) {
+          setProject(entries.items[0].fields);
+        } else {
+          setProject([]);
+        }
+      });
+  }, [props.slug]);
 
-  // if (project && project.length === 0) {
-  //   return <Redirect from="" to="/404" noThrow />;
-  // }
+  if (project && project.length === 0) {
+    return <Redirect from="" to="/404" noThrow />;
+  }
 
   return (
     <div className="SingleProject">
-      {/* <h1>{project && project.title}</h1>
+      <h1>{project && project.title}</h1>
       <img src={project && project.image.fields.file.url} alt={""} height></img>
-      {documentToReactComponents(project && project.text)} */}
+      {documentToReactComponents(project && project.text)}
     </div>
   );
 };
